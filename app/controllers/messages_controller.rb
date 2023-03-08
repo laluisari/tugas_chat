b2fa6b68c614799fbf2482dece1db45231037de3
laluisari <laluisari@gmail.com>
2023-03-08 14:24:12 +0700

    tugas

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,23 @@
+class MessagesController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    def index
+        render json: Message.all
+    end
+
+
+        def create 
+            msg = Message.new(message_params)
+            if msg.save
+              render json: msg, status: :created
+            else
+              render json: msg.errors, status: :unprocessable_entity
+            end
+        end
+          
+          private 
+          
+          def message_params
+            params.permit(:chat, :user_id, :image)
+          end
+
+end
